e5c77550ee145ab1f1f6ca19509b92936bbe4741
Faseeh Ahmad <faseehkamboh9933@gmail.com>
2025-12-25 22:35:50 +0500

    F2

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { emailOTPClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
 
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000", // the base url of your auth server
+    baseURL: process.env.BETTER_AUTH_URL || "https://classroom-ai-one.vercel.app", // the base url of your auth server
     plugins: [
         emailOTPClient(),
     ]
